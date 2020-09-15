2d14fd77fab98c2d50e237aa9577b04446d01b96
WarlianGratton <71021078+WarlianGratton@users.noreply.github.com>
2020-09-15 08:13:13 +0200

    test

---
+++ isnegative.go
@@ -0,0 +1,17 @@
+Raw
+Permalink
+Blame
+History
+  
+package IsNegative
+
+import "github.com/01-edu/z01"
+
+func IsNegative(nb int) {
+	if nb < 0 {
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
+	}
+	z01.PrintRune('\n')
+}

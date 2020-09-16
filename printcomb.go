2ee3bd98cbbdcc263a6a0e7d921d8aca0ef3b4d8
WarlianGratton <71021078+WarlianGratton@users.noreply.github.com>
2020-09-16 07:49:42 +0200

    test

--- printcomb.go
+++ printcomb.go
@@ -1,13 +1,20 @@
-package main
+package printcomb
 
 import "github.com/01-edu/z01"
 
-func main() {
-	var aRune rune= 'T'
-
-	z01.PrintRune(aRune)
-
-
-
-
+func PrintComb() {
+    for a := '0'; a <= '9'; a++ {
+        for b := a + 1; b <= '9'; b++ {
+            for c := b + 1; c <= '9'; c++ {
+                z01.PrintRune(a)
+                z01.PrintRune(b)
+                z01.PrintRune(c)
+                if a != '7' || b != '8' || c != '9' {
+                    z01.PrintRune(',')
+                    z01.PrintRune(' ')
+                }
+            }
+        }
+    }
+    z01.PrintRune(10)
 }
